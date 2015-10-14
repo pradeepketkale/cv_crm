7b70647514249b2842b7a5383980dfac9ba56246
Shruti Pande <shrutipande31@gmail.com>
2015-10-14 15:54:32 +0530

    error log added
    Former-commit-id: 148abe0f5e88058e629a4f036ed255baa16b2cb2

--- app/code/local/Craftsvilla/Generalcheck/Model/Observer.php
+++ app/code/local/Craftsvilla/Generalcheck/Model/Observer.php
@@ -9,7 +9,7 @@ public function crmTogetProductEventafter($observer)
 	$model= Mage::getStoreConfig('craftsvilla_config/service_api');
 	$url=$model['host'].':'.$model['port'].'/productUpdateNotification';
     	$data = array("productId"=>$productId); 
-	$data_string = json_encode($data);
+	$data_string = json_encode($data);//print_r($data_string);exit;
 	$handle = curl_init($url); 
 	curl_setopt($handle, CURLOPT_POST, true);
 	curl_setopt($handle, CURLOPT_POSTFIELDS, $data_string); 
@@ -17,7 +17,9 @@ public function crmTogetProductEventafter($observer)
 	    'Content-Type: application/json',                                                                                
 	    'Content-Length: ' . strlen($data_string))
 	    );
-	curl_exec($handle);
+	$response=curl_exec($handle);
+	$msg=$url.'|'.$productId.'|'.$response;
+	error_log($msg);
 	$http_status_code = curl_getinfo($handle, CURLINFO_HTTP_CODE);
 
 	if($http_status_code == 200){
@@ -25,9 +27,11 @@ public function crmTogetProductEventafter($observer)
 			return true;
 
 	}else{
+		
 			return false;
 			
 	}
+
 	
 	curl_close($handle);
 		
