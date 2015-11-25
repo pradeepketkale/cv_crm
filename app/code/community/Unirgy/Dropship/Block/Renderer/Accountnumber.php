acb7e1399349c39f84308b47f11af17d839d1650
dileswarp <dileswar@craftsvilla.com>
2015-11-25 11:09:26 +0530

    added try cacth to check decode failed error resolved or not ?
    Former-commit-id: a1d37eb2e6669da1d7c261dabf0846b0bc316b97

--- app/code/community/Unirgy/Dropship/Block/Renderer/Accountnumber.php
+++ app/code/community/Unirgy/Dropship/Block/Renderer/Accountnumber.php
@@ -2,8 +2,14 @@
 class Unirgy_Dropship_Block_Renderer_Accountnumber extends Mage_Adminhtml_Block_Widget_Grid_Column_Renderer_Abstract {
     public function render(Varien_Object $row)
     {
-        $data = Zend_Json::decode($row->getCustomVarsCombined());
+		try{        
+		$data = Zend_Json::decode($row->getCustomVarsCombined());
         return $data['bank_ac_number'];
+		}
+		catch (Exception $e) {
+			echo 'Failed';
+			}
+
     }
 }
 ?>
