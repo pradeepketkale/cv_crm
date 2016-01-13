5c4c96eb8e192244c144498263735afc9f307c58
dileswar <dileswar@craftsvilla.com>
2016-01-13 16:08:30 +0530

    shruti observer removed for cancel process
    Former-commit-id: 760f297d6b220459c1481669c025743906aad10a

--- app/code/local/Craftsvilla/Generalcheck/Model/Observer.php
+++ app/code/local/Craftsvilla/Generalcheck/Model/Observer.php
@@ -7,34 +7,29 @@ public function crmTogetProductEventafter($observer)
 	{
 		$pId = $observer->getEvent()->getProduct()->getId();
 		$productId=str_getcsv($pId,',','""');
-		$hlp = Mage::helper('generalcheck');
-		$hlp->productUpdateNotify_retry($productId);
+		 $model= Mage::getStoreConfig('craftsvilla_config/service_api');
+		 $url=$model['host'].':'.$model['port'].'/productUpdateNotification';
+
+		$data = array("productId"=>$productId,"apiVersionCode"=>"2"); 
+		$data_string = json_encode($data);//print_r($data_string);exit;
+		$handle = curl_init($url); 
+		curl_setopt($handle, CURLOPT_POST, true);
+		curl_setopt($handle, CURLOPT_POSTFIELDS, $data_string); 
+		curl_setopt($handle, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($handle, CURLOPT_HTTPHEADER, array('Content-Type: application/json','Content-Length: ' . strlen($data_string)));
+		curl_exec($handle);
+		//print_r($res);exit;
+		$http_status_code = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+		if($http_status_code == '200'){
+			return true;
+		}else{
+			return false;
+		}
+		curl_close($handle);
+		
 	}  
 
-public function crmTogetOrderCancelEventafter($obs){
-		//echo "hello";exit;		
-		$shipment = $obs->getEvent()->getShipment();
-		$shipmentId = $shipment->getEntityId();
-
-		 $_shipmentUdropshipstatus = $shipment->getUdropshipStatus();
-		 $statusCanceled = Unirgy_Dropship_Model_Source::SHIPMENT_STATUS_CANCELED;//exit;
-		$pid=array();
-		if($_shipmentUdropshipstatus == $statusCanceled){
-			    $items = $shipment->getAllItems();
-		foreach ($items as $item) {
-                    $productsToUpdate = $item->getProductId();
-			array_push($pid,$productsToUpdate);
-	            		 }
-
-			   $hlp = Mage::helper('generalcheck');
-	     		   $hlp->productUpdateNotify_retry($pid);
-		
-
-			}
-		
-
-
-	}       
+     
 
 }
 ?>
