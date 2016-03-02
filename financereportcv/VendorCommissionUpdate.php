10153f39aa9f347858e4a55d309aa6c0eb248a62
Ankit Gupta <ankit.gupta@craftsvilla.com>
2016-03-02 18:19:00 +0530

    variable commission bug corrected
    Former-commit-id: f3a71cf3efad3b8cddae34e9734f32be6a17628b

--- financereportcv/VendorCommissionUpdate.php
+++ financereportcv/VendorCommissionUpdate.php
@@ -112,9 +112,8 @@ include('session.php');
 	$sqlVendorComm = "SELECT `vendor_id`,`commission_percent`, `date_created` FROM `finance_vendor_commission` WHERE `vendor_id` = '".$vendor_id."' order by `date_created` desc ";
 	try{
 		$rVendor = mysql_query($sqlVendorComm,$mainConnection);
-		$rowCount = mysql_num_rows($rCheckVendor);
+		$rowCount = mysql_num_rows($sqlVendorComm);
 		$row = mysql_fetch_assoc($rVendor);
-		//var_dump($row['commission_percent']);
 		if($rowCount == 0){
 			$vendor_commission = 20;
 		} else {
--- financereportcv/session.php
+++ financereportcv/session.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
-$user_check=$_SESSION['login_user'];
-if(!isset($user_check)){
+if(!isset($_SESSION['login_user'])){
 /*	$writeCon->closeConnection();
 	$readcon->closeConnection();*/ // Closing Connection
 	header('Location: index.php'); // Redirecting To Home Page
