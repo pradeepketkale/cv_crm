d3186abb27c34a3051c6017f1f4fdf4edcc6c4d8
DurgeshParmar <durgesh.raj.parmar@gmail.com>
2016-05-18 18:55:56 +0530

    test module for admin panel
    Former-commit-id: 0b57f775a805d4a48ee3bf3ff375699101e0d491

---
+++ app/code/local/Craftsvilla/Seovendor/Block/Adminhtml/Seovendorbackend.php
@@ -0,0 +1,5 @@
+<?php  
+
+class Craftsvilla_Seovendor_Block_Adminhtml_Seovendorbackend extends Mage_Adminhtml_Block_Template {
+
+}
---
+++ app/code/local/Craftsvilla/Seovendor/controllers/Adminhtml/SeovendorbackendController.php
@@ -0,0 +1,10 @@
+<?php
+class Craftsvilla_Seovendor_Adminhtml_SeovendorbackendController extends Mage_Adminhtml_Controller_Action
+{
+	public function indexAction()
+    {
+       $this->loadLayout();
+	   $this->_title($this->__("Backend Page Title"));
+	   $this->renderLayout();
+    }
+}
