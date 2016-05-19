01bacb51c4a49c5529d0e69cd85357658640dd1b
DurgeshParmar <durgesh.raj.parmar@gmail.com>
2016-05-19 13:38:10 +0530

    disable seo vendor module
    Former-commit-id: d9a84595c5e8859be68fa0b0c049fd5f6077f15a

--- app/code/local/Craftsvilla/Seovendor/controllers/Adminhtml/SeovendorbackendController.php
+++ app/code/local/Craftsvilla/Seovendor/controllers/Adminhtml/SeovendorbackendController.php
@@ -1,18 +1,10 @@
 <?php
 class Craftsvilla_Seovendor_Adminhtml_SeovendorbackendController extends Mage_Adminhtml_Controller_Action
 {
-	protected function _initAction()
-	{
-		 $this->loadLayout()
-					->_setActiveMenu("seovendor/seovendor")
-					->_addBreadcrumb(Mage::helper("adminhtml")->__("VendorSeo  Manager"),Mage::helper("adminhtml")->__("VendorSeo Manager"));
-					return $this;
-	}
-	
-	public function indexAction() 
-	{
-			$this->_title($this->__("Manager Vendorseo"));
-			$this->_initAction();
+	public function indexAction()
+    {
+			$this->loadLayout();
+			$this->_title($this->__("Backend Page Title"));
 			$this->renderLayout();
-	}
+    }
 }
